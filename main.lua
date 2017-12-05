355ad97957182f241a0d2fbaef1bc5c0c966059a
Nikki Koole <nikkikoole@gmail.com>
2017-12-05 08:32:22 +0100

    added vendor and fonts

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@
    todo polygon is broken; cannot add new vertices
 ]]--
 
+local suit = require 'vendor.suit'
 Signal = require 'vendor.signal'
 Gamestate = require "vendor.gamestate"
 Camera = require "vendor.camera"
@@ -16,12 +17,17 @@ local utils = require "utils"
 local shapes = require "shapes"
 poly = require 'poly'
 
-
-
+-- storage for text input
+local input = {text = ""}
+local slider = {value = 1, min = 0, max = 2}
+local checkbox = {checked = true, text="stuff"}
 
 function love.load()
    if arg[#arg] == "-debug" then require("mobdebug").start() end
    love.window.setMode(1024, 768, {resizable=true, vsync=true, fullscreen=false})
+
+   icon_font = love.graphics.newFont("icons.ttf", 30)
+   helvetica = love.graphics.newFont("helvetica_bold.ttf", 18)
 
    world = {
       children={
@@ -105,6 +111,8 @@ function love.keypressed(key)
       local data = love.filesystem.newFileData("filename.txt")
       world = loadstring("return "..data:getString())()
    end
+   	suit.keypressed(key)
+
 end
 
 function love.filedropped(file)
@@ -112,6 +120,25 @@ function love.filedropped(file)
    world = loadstring("return "..data:getString())()
    --world = bitser.loadData(data:getPointer(), data:getSize())
 end
+
+local bytemarkers = { {0x7FF,192}, {0xFFFF,224}, {0x1FFFFF,240} }
+function utf8(decimal)
+   if decimal<128 then return string.char(decimal) end
+   local charbytes = {}
+   for bytes,vals in ipairs(bytemarkers) do
+      if decimal<=vals[1] then
+         for b=bytes+1,2,-1 do
+            local mod = decimal%64
+            decimal = (decimal-mod)/64
+            charbytes[b] = string.char(128+mod)
+         end
+         charbytes[1] = string.char(vals[2]+decimal)
+         break
+      end
+   end
+   return table.concat(charbytes)
+end
+
 
 function love.update(dt)
    if love.keyboard.isDown("escape") then love.event.quit() end
@@ -126,6 +153,54 @@ function love.update(dt)
             c.triangles = poly.triangulate(c.type, shape)
       end
    end
+   love.graphics.setFont(helvetica)
+
+   suit.layout:reset(10,100)
+   suit.layout:padding(10,10)
+	-- put an input widget at the layout origin, with a cell size of 200 by 30 pixels
+	suit.Input(input, suit.layout:row(200,30))
+
+	-- put a label that displays the text below the first cell
+	-- the cell size is the same as the last one (200x30 px)
+	-- the label text will be aligned to the left
+	suit.Label("Hello, "..input.text, {align = "left", color={255,0,0}}, suit.layout:row())
+
+	-- put an empty cell that has the same size as the last cell (200x30 px)
+	-- put a button of size 200x30 px in the cell below
+	-- if the button is pressed, quit the game
+    --local str =  '\\u{e87C}'
+    --local str= utf8(0xE869)
+    love.graphics.setFont(icon_font)
+
+    local str = utf8(0xE40A)
+	if suit.Button(str, suit.layout:row()).hit then
+		love.event.quit()
+	end
+    --love.graphics.setFont(love.graphics.newFont(18))
+   love.graphics.setFont(helvetica)
+
+    if suit.Button("Blaargh", suit.layout:row()).hit then
+       suit.Label("Urgh?", suit.layout:row())
+
+	end
+
+    suit.layout:left(300,30)
+    suit.layout:row()
+    --suit.layout:reset()
+
+	suit.Button("whaja", suit.layout:col(100,30))
+	suit.Button("onka", suit.layout:col(30,30))
+    suit.Button("duff", suit.layout:col(50,30))
+
+    suit.layout:left(140,30)
+    suit.layout:row(200,30)
+
+
+
+    suit.Slider(slider, suit.layout:row())
+    suit.Checkbox(checkbox, suit.layout:row())
+
+    suit.Label(tostring(slider.value), suit.layout:row())
 end
 
 function love.draw()
@@ -146,4 +221,17 @@ function love.draw()
 
    camera:detach()
    love.graphics.print("#tris "..triangle_count, 10, 30)
+
+   suit.draw()
+
+end
+
+function love.textedited(text, start, length)
+    -- for IME input
+    suit.textedited(text, start, length)
+end
+
+function love.textinput(t)
+	-- forward text input to SUIT
+	suit.textinput(t)
 end
